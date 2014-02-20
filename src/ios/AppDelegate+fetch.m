14b34b06b27c116ff43b794a82741d8de33a4eeb
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-02-20 09:38:00 +0100

    initial version

---
+++ src/ios/AppDelegate+fetch.m
@@ -0,0 +1,62 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+*/
+#import "AppDelegate+fetch.h"
+#import "BackgroundPlugin.h"
+#import <objc/runtime.h>
+
+@implementation AppDelegate (fetch)
+
+- (id)getCommandInstance:(NSString *)className {
+    return [self.viewController getCommandInstance:className];
+}
+
+// its dangerous to override a method from within a category.
+// Instead we will use method swizzling. we set this up in the load call.
++ (void)load {
+    Method original, swizzled;
+
+    original = class_getInstanceMethod(self, @selector(init));
+    swizzled = class_getInstanceMethod(self, @selector(swizzled_init));
+    method_exchangeImplementations(original, swizzled);
+}
+
+- (AppDelegate *)swizzled_init {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(registerBackgroundFetch:)
+                                                 name:@"UIApplicationDidFinishLaunchingNotification" object:nil];
+
+    // This actually calls the original init method over in AppDelegate. Equivalent to calling super
+    // on an overrided method, this is not recursive, although it appears that way. neat huh?
+    return [self swizzled_init];
+}
+
+// This code will be called immediately after application:didFinishLaunchingWithOptions:.
+- (void)registerBackgroundFetch:(NSNotification *)notification {
+    [[UIApplication sharedApplication] setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+}
+
+- (void) application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    BackgroundPlugin *plugin = [self getCommandInstance:@"BackgroundFetch"];
+    [plugin performSelectorOnMainThread:@selector(backgroundFetch:) withObject:completionHandler waitUntilDone:YES];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler {
+    [self application:application performFetchWithCompletionHandler:completionHandler];
+}
+
+@end
---
+++ www/backgroundFetch.js
@@ -0,0 +1,66 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+var exec = require('cordova/exec');
+
+/**
+ * This is a global variable called backgroundFetch exposed by cordova
+ * @status Stable
+ * @constructs backgroundFetch
+ */
+var backgroundFetch = {
+
+    /**
+     * Registers the device for background fetch.
+     * @param {Function} success - callback to be executed to check if there is content availble
+     * @returns {void}
+     */
+    register: function(callback) {
+        return exec(callback, null, 'BackgroundFetch', 'register', []);
+    },
+
+    /**
+     * Call this function to tell the OS if there was data or not so it can schedule the next fetch operation
+     * @param {int} dataType - one of the BackgroundFetchResults or 0 new data 1 no data or 2 failed
+     * @returns {void}
+     * @example
+        
+        backgroundFetch.register(function() {
+            console.log('fetching stuff in background');
+            //... actual fetching
+            // if depening if there was new data or not...
+            backgroundFetch.setContentAvailable(backgroundFetch.BackgroundFetchResult.NewData);
+        });
+     */
+    setContentAvailable: function(dataType) {
+        return exec(null, null, "BackgroundFetch", "setContentAvailable", [{type: dataType}]);
+    },
+
+    BackgroundFetchResult: {
+        NewData: 0,
+        NoData: 1,
+        Failed: 2
+    }
+};
+
+if (Object.freeze) {
+    Object.freeze(backgroundFetch.BackgroundFetchResult);
+}
+
+module.exports = backgroundFetch;
